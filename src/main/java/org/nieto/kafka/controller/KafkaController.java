b7c1085392c216ccb7010c1d5aee259aefd0289b
Antonio Nieto García <antonio.nieto.garcia@externos.gloval.es>
2022-01-14 09:40:46 +0100

    first commit

---
+++ src/main/java/org/nieto/kafka/controller/KafkaController.java
@@ -0,0 +1,26 @@
+package org.nieto.kafka.controller;
+
+import org.nieto.kafka.service.KafkaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class KafkaController {
+
+	@Autowired
+	private KafkaService kafkaService;
+
+	@GetMapping
+	public ResponseEntity<String> hello() {
+		return ResponseEntity.ok("Welcome to java-kafka-test");
+	}
+
+	@PostMapping
+	public ResponseEntity<String> sendMessage(@RequestBody String message) {
+		kafkaService.sendMessage(message);
+		return ResponseEntity.ok("Ejecutado");
+	}
+
+}
---
+++ src/main/java/org/nieto/kafka/service/KafkaService.java
@@ -0,0 +1,42 @@
+package org.nieto.kafka.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Service
+public class KafkaService {
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	@Value(value = "${kafka.topic}")
+	private String topic;
+
+	public void sendMessage(String msg) {
+		ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, msg);
+
+		ListenableFutureCallback<? super SendResult<String, String>> listenable = new ListenableFutureCallback<SendResult<String, String>>() {
+
+			@Override
+			public void onSuccess(SendResult<String, String> result) {
+				System.out.println("Sent message with offset=[" + result.getRecordMetadata().offset() + "]");
+//	            System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+			}
+
+			@Override
+			public void onFailure(Throwable ex) {
+				System.out.println("Unable to send message due to : " + ex.getMessage());
+//	            System.out.println("Unable to send message=["+ message + "] due to : " + ex.getMessage());
+			}
+		};
+
+		future.addCallback(listenable);
+
+	}
+
+}
